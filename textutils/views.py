92cac1fb1b3a2b0fb6373849a6b70537a6dff793
Vishal Kumar <kvishalrj2020@gmail.com>
2023-09-07 15:56:55 +0530

    second commit

--- textutils/views.py
+++ textutils/views.py
@@ -3,8 +3,27 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    params = {'name':'Vishal', 'place':'Mars'}
-    return render(request, 'index.html', params)
+    # params = {'name':'Vishal', 'place':'Mars'}
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("Hello About")
+
+def analyze(request):
+    djtext = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    print(djtext)
+    print(removepunc)
+    if removepunc=='on':
+        punctuations = '''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed += char
+
+        params = {'purpose':'Remove punctuation',
+                'analyzed_text':analyzed}
+        return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse('Please write a sentence')
+
